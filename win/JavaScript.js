a68ee4699e09fb25c9397467293dad11d6199577
Kubson3000 <Kubson30003@gmail.com>
2022-05-30 06:41:48 +0200

    second page

---
+++ win/JavaScript.js
@@ -0,0 +1,43 @@
+function preview(number) {
+    switch(number) {
+        case 1:
+            let des = document.getElementById("preview_div");
+            des.innerHTML = "<img class=\"cen itm\" src=\"./gui/win1.png\" />";
+            document.getElementById(number).classList.add("active");
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        case 6:
+            break;
+        case 7:
+            break;
+        case 8:
+            break;
+        case 9:
+            break;
+        case 10:
+            break;
+        case 11:
+            break;
+        case 12:
+            break;
+        case 13:
+            break;
+        case 14:
+            break;
+        case 15:
+            break;
+        case 16:
+            break;
+        case 17:
+            break;
+        case 18:
+            break;
+    }
+}
