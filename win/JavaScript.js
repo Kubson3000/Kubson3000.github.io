a0846a786c60f6d566a6b542e7141a629c21986f
Kubson3000 <Kubson30003@gmail.com>
2022-05-30 16:15:17 +0200

    last upping

--- win/JavaScript.js
+++ win/JavaScript.js
@@ -9,58 +9,58 @@ function preview(number) {
     document.getElementById(number).classList.add("active");
     switch(number) {
         case 1:
-            des.innerHTML = "<img class=\"cen itm\" src=\"./gui/win1.png\" />";
+            des.innerHTML = "<img class=\"cen wha itm\" src=\"./gui/win1.png\" />";
             break;
         case 2:
-            des.innerHTML = "<img class=\"cen itm\" src=\"./gui/win2.png\" />";
+            des.innerHTML = "<img class=\"cen wha itm\" src=\"./gui/win2.png\" />";
             break;
         case 3:
-            des.innerHTML = "<img class=\"cen itm\" src=\"./gui/win2.1.png\" />";
+            des.innerHTML = "<img class=\"cen wha itm\" src=\"./gui/win2.1.png\" />";
             break;
         case 4:
-            des.innerHTML = "<img class=\"cen itm\" src=\"./gui/win3.png\" />";
+            des.innerHTML = "<img class=\"cen wha itm\" src=\"./gui/win3.png\" />";
             break;
         case 5:
-            des.innerHTML = "<img class=\"cen itm\" src=\"./gui/win3.1.png\" />";
+            des.innerHTML = "<img class=\"cen wha itm\" src=\"./gui/win3.1.png\" />";
             break;
         case 6:
-            des.innerHTML = "<img class=\"cen itm\" src=\"./gui/win_nt3.1.png\" />";
+            des.innerHTML = "<img class=\"cen wha itm\" src=\"./gui/win_nt3.1.png\" />";
             break;
         case 7:
-            des.innerHTML = "<img class=\"cen itm\" src=\"./gui/win_nt3.5.png\" />";
+            des.innerHTML = "<img class=\"cen wha itm\" src=\"./gui/win_nt3.5.png\" />";
             break;
         case 8:
-            des.innerHTML = "<img class=\"cen itm\" src=\"./gui/win_nt4.0.png\" />";
+            des.innerHTML = "<img class=\"cen wha itm\" src=\"./gui/win_nt4.0.png\" />";
             break;
         case 9:
-            des.innerHTML = "<img class=\"cen itm\" src=\"./gui/win98.png\" />";
+            des.innerHTML = "<img class=\"cen wha itm\" src=\"./gui/win98.png\" />";
             break;
         case 10:
-            des.innerHTML = "<img class=\"cen itm\" src=\"./gui/win2000.png\" />";
+            des.innerHTML = "<img class=\"cen wha itm\" src=\"./gui/win2000.png\" />";
             break;
         case 11:
-            des.innerHTML = "<img class=\"cen itm\" src=\"./gui/win_me.png\" />";
+            des.innerHTML = "<img class=\"cen wha itm\" src=\"./gui/win_me.png\" />";
             break;
         case 12:
-            des.innerHTML = "<img class=\"cen itm\" src=\"./gui/win_xp.png\" />";
+            des.innerHTML = "<img class=\"cen wha itm\" src=\"./gui/win_xp.png\" />";
             break;
         case 13:
-            des.innerHTML = "<img class=\"cen itm\" src=\"./gui/win_vista.png\" />";
+            des.innerHTML = "<img class=\"cen wha itm\" src=\"./gui/win_vista.png\" />";
             break;
         case 14:
-            des.innerHTML = "<img class=\"cen itm\" src=\"./gui/win7.png\" />";
+            des.innerHTML = "<img class=\"cen wha itm\" src=\"./gui/win7.png\" />";
             break;
         case 15:
-            des.innerHTML = "<img class=\"cen itm\" src=\"./gui/win8.png\" />";
+            des.innerHTML = "<img class=\"cen wha itm\" src=\"./gui/win8.png\" />";
             break;
         case 16:
-            des.innerHTML = "<img class=\"cen itm\" src=\"./gui/win8.1.png\" />";
+            des.innerHTML = "<img class=\"cen wha itm\" src=\"./gui/win8.1.png\" />";
             break;
         case 17:
-            des.innerHTML = "<img class=\"cen itm\" src=\"./gui/win10.png\" />";
+            des.innerHTML = "<img class=\"cen wha itm\" src=\"./gui/win10.png\" />";
             break;
         case 18:
-            des.innerHTML = "<img class=\"cen itm\" src=\"./gui/win11.png\" />";
+            des.innerHTML = "<img class=\"cen wha itm\" src=\"./gui/win11.png\" />";
             break;
     }
 }
